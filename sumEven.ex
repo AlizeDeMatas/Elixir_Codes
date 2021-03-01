2786568236d6407482fedfb4b266fce9c0fa5b17
AlizeDeMatas <32222634+AlizeDeMatas@users.noreply.github.com>
2021-02-28 20:55:21 -0500

    Create sumEven.ex
        # sumEven–Return the sum of all even integers in an input list.

---
+++ sumEven.ex
@@ -0,0 +1,15 @@
+# sumEven–Return the sum of all even integers in an input list.
+    # Helper Function 
+    def sumEven(list), do: sumEven(list,0)
+    # Base Case
+    def sumEven([], sum), do: sum
+
+    # Recursive Call
+    def sumEven([h | t], sum) do
+        element_good = is_integer(h) && rem(h, 2) == 0
+        if element_good do 
+            sumEven(t, sum+h)
+        else 
+            sumEven(t, sum)
+        end 
+    end 
